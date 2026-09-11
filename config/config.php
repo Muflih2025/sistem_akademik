915f55509e3073c80532c00e3276232a07529075
Muflih <muflih.rafileseppa@gmail.com>
2026-09-11 08:41:27 +0700

    fix:

--- config/config.php
+++ config/config.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 const APP_NAME = 'SIA SMKN 2 Magelang';
 const DB_HOST = '127.0.0.1';
+const DB_PORT = 3307;
 const DB_NAME = 'db_sia_smkn2_magelang';
 const DB_USER = 'root';
 const DB_PASS = '';
@@ -16,7 +17,7 @@ if (session_status() !== PHP_SESSION_ACTIVE) {
 function db(): PDO {
     static $pdo;
     if (!$pdo) {
-        $pdo = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME.';charset=utf8mb4', DB_USER, DB_PASS, [
+        $pdo = new PDO('mysql:host='.DB_HOST.';port='.(defined('DB_PORT') ? DB_PORT : 3306).';dbname='.DB_NAME.';charset=utf8mb4', DB_USER, DB_PASS, [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
             PDO::ATTR_EMULATE_PREPARES => false,
